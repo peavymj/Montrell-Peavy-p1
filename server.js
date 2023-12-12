bb58ae1f50757ef7011e61eb5ed3e8b9f8fb5fd4
Montrell <peavymj@gmatc.matc.edu>
2023-12-11 19:15:56 -0600

    Added database connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 const app = express();
 
 
+
+
+connectDatabase();
 
 app.get('/', (req,res) =>
     res.send('http get request sent to root api endpoint')
