d21dbd44772e3554b4550dc3330bb8ffeab6e98a
Montrell <peavymj@gmatc.matc.edu>
2023-12-11 21:04:47 -0600

    Completed Project 1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
-
+import {check, validationResult } from 'express-validator';
 const app = express();
 
 
@@ -8,9 +8,30 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req,res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+app.post('/api/users',
+[
+    check('name', 'Please enter your name')
+    .not()
+    .isEmpty(),
+    check('Age', 'Please enter your age').isInt({min: 18}),
+    check(
+    'phone number',
+     'Your number wont be visible to other users')
+    .isMobilePhone()
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+});
 
-app.listen(3000, () => console.log('Express server running on port 3000'));
+app.listen(4000, () => console.log('Express server running on port 4000'));
